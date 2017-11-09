46457c519c97dddd2be4139ecf92659c0dac4cb0
toshi0383 <t.suzuki326@gmail.com>
2017-11-09 13:24:00 +0900

    Fix ViewAsOverlay not scaled correctly on tvOS10 or earlier where ViewAsImage was ok

--- ImageOverlay/OverlayProtocol.swift
+++ ImageOverlay/OverlayProtocol.swift
@@ -11,14 +11,16 @@ import Foundation
 public protocol OverlayProtocol {
     var layers: [CALayer] { get }
 
-    @available(tvOS 11.0, *)
     var needsRendering: Bool { get }
 }
 
 extension OverlayProtocol {
-    @available(tvOS 11.0, *)
     public var needsRendering: Bool {
-        return false
+        if #available(tvOS 11.0, *) {
+            return false
+        } else {
+            return true
+        }
     }
 }
 
