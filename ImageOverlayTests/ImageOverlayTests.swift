4cba333641a7d61a4457600d96470d6609be726d
toshi0383 <t.suzuki326@gmail.com>
2017-11-10 13:44:50 +0900

    Add AnyViewAsOverlayTests

---
+++ ImageOverlayTests/ImageOverlayTests.swift
@@ -0,0 +1,18 @@
+//
+//  ImageOverlayTests.swift
+//  ImageOverlayTests
+//
+//  Created by Toshihiro Suzuki on 2017/11/10.
+//  Copyright © 2017 toshi0383. All rights reserved.
+//
+
+import XCTest
+
+class ImageOverlayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    func testExample() {
+    }
+}
